43200dac209312d71fd6640f2f63ab1fca81ec37
Evgeny Tupikov <bwinter@yandex.ru>
2021-03-12 01:50:51 +0500

    User page improve

--- src/app/pages/inner-pages/users-page/user/user.component.ts
+++ src/app/pages/inner-pages/users-page/user/user.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { User } from 'src/app/models/user.model';
+import { LoadingController } from '@ionic/angular';
+
+import { User } from '../../../../models/user.model';
 import { API_URL, HttpHelperService } from '../../../../services/http-helper.service';
 
 @Component({
@@ -8,35 +10,35 @@ import { API_URL, HttpHelperService } from '../../../../services/http-helper.ser
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
 })
-export class UserComponent implements OnInit {
+export class UserComponent {
 
   user: User = null;
   apiUploads = API_URL + '/uploads/';
 
   constructor(
     private route: ActivatedRoute,
-    private http: HttpHelperService
+    private http: HttpHelperService,
+    private loadingController: LoadingController
   ) { }
 
-  ngOnInit() {
-    console.log('ngOnInit');
-  }
-
   ionViewDidEnter() {
-    console.log('ionViewDidEnter');
     this.getUsers();
   }
 
   ionViewWillLeave() {
-    console.log('ionViewWillLeave');
     this.user = null;
   }
 
-  private getUsers() {
+  private async getUsers() {
+    const loading = await this.loadingController.create({message: 'Please wait...'});
+    await loading.present();
     const id = this.route.snapshot.params?.id;
-    this.http.get<User>('users/' + id).subscribe(user => {
+    this.http.get<User>('users/' + id).subscribe(async user => {
       this.user = user;
-    }, () => { });
+      await loading.dismiss();
+    }, async () => {
+      await loading.dismiss();
+    });
   }
 
 }
